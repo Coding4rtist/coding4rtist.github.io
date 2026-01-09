2ae3ddc08b8192bab55868e3b27108d7beb69a4d
Coding Artist <coding4rtist@gmail.com>
2026-01-10 00:58:52 +0100

    Fix

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -6,14 +6,4 @@
 
 // You can delete this file if you're not using it
 
-import React from 'react';
-import { GlobalProvider } from './src/context/globalContext';
-
-import './src/styles/global.css';
-import './src/styles/embla.css';
-
-export const wrapRootElement = ({ element }) => {
-	return <GlobalProvider>{element}</GlobalProvider>;
-};
-
 import './src/styles/global.css';
--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -8,11 +8,4 @@
  * @type {import('gatsby').GatsbySSR['onRenderBody']}
  */
 
-import React from 'react';
-import { GlobalProvider } from './src/context/globalContext';
-
-export const wrapRootElement = ({ element }) => {
-	return <GlobalProvider>{element}</GlobalProvider>;
-};
-
 import './src/styles/global.css';
